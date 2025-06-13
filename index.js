dc99e284da1608d3cb6ca1e014bc76dea1c4de8d
Rithick-gitup <rithickk27@gamil.com>
2025-06-13 18:49:52 +0530

    changes are made

---
+++ index.js
@@ -0,0 +1,12 @@
+var sidenav = document.getElementById("side-navbar")
+
+
+function showNavbar()
+{
+sidenav.style.display="block"
+}
+
+function closeNavbar()
+{
+     sidenav.style.display="none"
+}
